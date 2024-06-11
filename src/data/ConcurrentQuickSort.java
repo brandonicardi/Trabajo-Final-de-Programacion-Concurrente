62501b1a77614fcb9f21aeb91ce733557c7422dd
brandonicardi <brandonicardi@gmail.com>
2024-06-10 23:03:55 -0300

    Se desarrollaron ambos algoritmos junto con la clase Test, falta comentar cada paso para mejorar su comprension.

--- src/data/ConcurrentQuickSort.java
+++ src/data/ConcurrentQuickSort.java
@@ -1,5 +1,70 @@
 package data;
 
-public class ConcurrentQuickSort {
+import java.util.concurrent.RecursiveTask;
+import java.util.Random;
 
+@SuppressWarnings("serial")
+public class ConcurrentQuickSort extends RecursiveTask<Void> {
+    private int[] array;
+    private int start;
+    private int end;
+
+    private int partition(int start, int end, int[] arr) {
+        int i = start, j = end;
+        int pivoted = new Random().nextInt(j - i) + i;
+        int t = arr[j];
+        arr[j] = arr[pivoted];
+        arr[pivoted] = t;
+        j--;
+
+        while (i <= j) {
+            if (arr[i] <= arr[end]) {
+                i++;
+                continue;
+            }
+
+            if (arr[j] >= arr[end]) {
+                j--;
+                continue;
+            }
+
+            t = arr[j];
+            arr[j] = arr[i];
+            arr[i] = t;
+            j--;
+            i++;
+        }
+
+        t = arr[j + 1];
+        arr[j + 1] = arr[end];
+        arr[end] = t;
+        return j + 1;
+    }
+
+    public ConcurrentQuickSort(int[] array, int start, int end) {
+        this.array = array;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Void compute() {
+        if (start >= end || array == null || array.length == 0)
+            return null;
+
+        if (start < 0 || end >= array.length)
+            return null;
+
+        int p = partition(start, end, array);
+
+        ConcurrentQuickSort left = new ConcurrentQuickSort(array, start, p - 1);
+        ConcurrentQuickSort right = new ConcurrentQuickSort(array, p + 1, end);
+
+        left.fork();
+        right.compute();
+
+        left.join();
+
+        return null;
+    }
 }
--- src/test/AlgorithmTest.java
+++ src/test/AlgorithmTest.java
@@ -1,10 +1,56 @@
 package test;
 
-public class AlgorithmTest {
+import data.ConcurrentQuickSort;
+import data.SequentialQuicksort;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ForkJoinPool;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+public class AlgorithmTest 
+{
+    public static void main(String[] args) 
+    {
+        int n = 100000000;
+    
+        if(n <= 1) 
+        {
+            System.out.println("El array no contiene elementos suficientes para ser ordenado.");
+            return;
+        }
+    
+        int[] arr = new int[n];
+        Random random = new Random();
+    
+        for(int i = 0; i < n; i++) 
+        {
+            arr[i] = random.nextInt(n + 1);
+        }     
+    
+        
+        int arrayOrdenarSecuencialmente[] = Arrays.copyOf(arr, n);
+        int arrayOrdenarConcurrententemente[] = Arrays.copyOf(arr, n);
+        
+        SequentialQuicksort qss = new SequentialQuicksort();
+        
+        long tiempoInicialSecuencial = System.nanoTime();
+        
+        qss.quickSort(arrayOrdenarSecuencialmente, 0, n - 1);
+        
+        long tiempoFinalSecuencial = System.nanoTime() - tiempoInicialSecuencial;
+        
+        
+        ForkJoinPool pool = new ForkJoinPool();
+        
+        ConcurrentQuickSort qsc = new ConcurrentQuickSort(arrayOrdenarConcurrententemente, 0, n - 1); // Modificado
+        
+        long tiempoInicialConcurrente = System.nanoTime();
+        
+        pool.invoke(qsc);
+        
+        long tiempoFinalConcurrente = System.nanoTime() - tiempoInicialConcurrente;
+      
+        System.out.println("Para ordenar " + n + " elementos:");
+        System.out.println("El algoritmo QuickSort SECUENCIAL tardó: " + tiempoFinalSecuencial / 1000000 + " ms.");
+        System.out.println("El algoritmo QuickSort CONCURRENTE tardó: " + tiempoFinalConcurrente / 1000000 + " ms.");
+    }
 }
